af6bb1dd3b0fa00b1d71c71d0b0e966469402550
Calvin Carter <calvin@wifi-10-44-252-196.wifi.berkeley.edu>
2024-02-20 14:20:05 -0800

    commit

--- R/favorite_artists.R
+++ R/favorite_artists.R
@@ -1,16 +1,15 @@
-#' Count the number of artists in your playlist!
+#' Count the number of artists in your playlist
 #'
 #' @param spotify_data A data.frame from exportify.net
-#' @param count An integer who dictates the number of rows
-#' @param see_all A Boolean who dictates if the function should return all available rows
+#' @param count An integer indicating the number of rows to return
+#' @param see_all A Boolean indicating if the function should return all available rows
+#' @param ascending A Boolean who orders the resulting data.frame based on num_appearances
 #'
 #' @return A data.frame ordering the amount of times each artist appears in a data.frame
 #' @export
 #'
 #' @examples
-#' my_playlist <- readr::read_csv('../randb.csv')
-#' favorite_artists(spotify_data = my_playlist)
-#' favorite_artists(my_playlist, count = 5L)
+#' a = "WIP"
 #'
 favorite_artists <- function(spotify_data, count = 6L, see_all = FALSE, ascending = FALSE) {
   if (!is.data.frame(spotify_data)) {
@@ -33,12 +32,12 @@ favorite_artists <- function(spotify_data, count = 6L, see_all = FALSE, ascendin
 artist_breakdown <- function(spotify_data, ascending) {
   result <- spotify_data |>
     dplyr::select(`Artist Name(s)`) |>
-    dplyr::mutate(artist = (str_split(`Artist Name(s)`, ",(?=\\S)"))) |>
+    dplyr::mutate(artist = (stringr::str_split(`Artist Name(s)`, ",(?=\\S)"))) |>
     tidyr::unnest(artist) |>
     dplyr::group_by(artist) |>
     dplyr::summarize(num_appearances = dplyr::n())
   if (ascending) {
     return (result |> dplyr::arrange(num_appearances))
   }
-  return (result |> dplyr::arrange(desc(num_appearances)))
+  return (result |> dplyr::arrange(dplyr::desc(num_appearances)))
 }
