1634cc597dda45b381ecd8bca28e201c691fa1f2
Calvin Carter <calvin@Calvins-MacBook-Air.local>
2024-03-05 13:13:35 -0800

    added data documentation

---
+++ R/rb.R
@@ -0,0 +1,13 @@
+#' Exporify Spotify Data
+#'
+#' Data from the author's R&B Playlist
+#'
+#' @format ## `rb`
+#' A data frame with 46 rows and 23 columns:
+#' \describe{
+#'   \item{Genres}{genre names}
+#'   ...
+#' }
+#' @source <https://exportify.net/>
+"rb"
+
